5925d8f0c9d85f85dd9a6f98d0cb20d47abfd26e
DuckGu <moonnn@hanmail.net>
2023-02-06 05:11:21 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default function App() {
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
       <GundamHeader />
+      <GundamHeader />
     </div>
   );
 }
